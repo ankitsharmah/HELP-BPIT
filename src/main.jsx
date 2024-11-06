baaf49ff76937eed24329ed9743386f7fa0aa6df
ankitsharma06 <asrover9810@gmail.com>
2024-11-06 22:45:24 +0530

    some update code

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
+
 import store from './redux/store.js'
 // export const BASE_URL="http://localhost:9191"
 export const BASE_URL="https://help-bipit-api.onrender.com"
