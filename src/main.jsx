63bb48fa233b5e5eed0033f02e93fd64eaa9f473
ankitsharma06 <asrover9810@gmail.com>
2024-11-20 21:32:24 +0530

    api changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux'
 
 import store from './redux/store.js'
 import SearchSuggestions from './components/SearchSuggestion.jsx'
-export const BASE_URL="http://localhost:9191"
-// export const BASE_URL="https://help-bipit-api.onrender.com"
+// export const BASE_URL="http://localhost:9191"
+export const BASE_URL="https://help-bipit-api.onrender.com"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
