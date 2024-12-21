793a94e47ca9177a1fa39fff45f5cc97fffb9518
ankitsharma06 <asrover9810@gmail.com>
2024-12-21 15:50:04 +0530

    report item api implimented

--- src/components/LostAndFooundReport.jsx
+++ src/components/LostAndFooundReport.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setAllFilterData, setFilterdata } from "../redux/lostAndFound";
+import { setAllFilterData, setFilterdata, setReports } from "../redux/lostAndFound";
+import axios from "axios";
+import { BASE_URL } from "../main";
+import Header from "../shared/Header";
 
 const LostAndFoundReport = () => {
   const navigate = useNavigate();
   const [isReporting, setIsReporting] = useState(false);
   const dispatch = useDispatch();
-  
+  const [addingReport,setAddReport]= useState(false);
+
   const { allReports } = useSelector((store) => store.reports);
   const { isLoadingReports } = useSelector((store) => store.reports);
   const { filterData } = useSelector((store) => store.reports);
@@ -25,7 +29,7 @@ const LostAndFoundReport = () => {
   });
   useEffect(() => {
     function filterReports() {
-      console.log("Filtering with:", filterData);
+
 
       if (filterData.category || filterData.date || filterData.status) {
         const newFilteredReports = allReports.filter((report) => {
@@ -35,7 +39,9 @@ const LostAndFoundReport = () => {
           const matchesDate = filterData.date
             ? report.foundOn === filterData.date
             : true;
-          const matchesStatus = filterData.status ? report.reportStatus === filterData.status : true;
+          const matchesStatus = filterData.status
+            ? report.reportStatus === filterData.status
+            : true;
           return matchesCategory && matchesDate && matchesStatus;
         });
 
@@ -48,29 +54,49 @@ const LostAndFoundReport = () => {
     if (allReports) {
       filterReports();
     }
-
-  }, [filterData, allReports,dispatch]);
+  }, [filterData, allReports, dispatch]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("Submitted data:", formData);
+    setAddReport(true);
+
+    const res = await axios.post(`${BASE_URL}/api/v1/reports/report-item`, formData,     {
+      headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      withCredentials: true,
+  })
+ 
+  if(res.data.success){
+    const updatedReports = [...allReports,res.data.reports];
+    console.log(updatedReports)
+    dispatch(setReports(updatedReports))
+    setAddReport(false)
     setIsReporting(false);
+  }
     // TODO: Send `formData` to the backend using axios or fetch
   };
 
   return (
     <div className="relative text-white h-[86vh] md:h-[91vh] p-4 sm:p-6">
       {/* Background content */}
-      <div className={`flex h-[85vh] lg:grid lg:grid-cols-4 md:gap-4 ${isReporting ? "blur-sm" : ""}`}>
+      <div
+        className={`flex h-[85vh] lg:grid lg:grid-cols-4 md:gap-4 ${
+          isReporting ? "blur-sm" : ""
+        }`}
+      >
         {/* Sidebar */}
         <aside className="space-y-4 border-r-[1px] pr-3 w-[54%] md:w-full lg:col-span-1">
           <div>
-            <label className="block mb-2 text-sm font-medium">Filter by Category</label>
+            <label className="block mb-2 text-sm font-medium">
+              Filter by Category
+            </label>
             <select
               onChange={(e) =>
                 dispatch(
@@ -90,7 +116,9 @@ const LostAndFoundReport = () => {
             </select>
           </div>
           <div>
-            <label className="block mb-2 text-sm font-medium">Filter by Date</label>
+            <label className="block mb-2 text-sm font-medium">
+              Filter by Date
+            </label>
             <input
               onChange={(e) =>
                 dispatch(
@@ -120,7 +148,7 @@ const LostAndFoundReport = () => {
                   }
                   value={filterData.status}
                   type="radio"
-                  checked={filterData.status==="LOST"}
+                  checked={filterData.status === "LOST"}
                   name="status"
                   className="text-cyan-500"
                 />
@@ -135,11 +163,9 @@ const LostAndFoundReport = () => {
                         val: "FOUND",
                       })
                     )
-                  } 
-                 
+                  }
                   type="radio"
-                  checked={filterData.status==="FOUND"}
-
+                  checked={filterData.status === "FOUND"}
                   name="status"
                   className="text-cyan-500"
                 />
@@ -147,9 +173,14 @@ const LostAndFoundReport = () => {
               </label>
             </div>
 
-           <div className="text-center">
-           <button className="mt-5 text-center " onClick={()=>dispatch(setAllFilterData())}>clear filter</button>
-           </div>
+            <div className="text-center">
+              <button
+                className="mt-5 text-center "
+                onClick={() => dispatch(setAllFilterData())}
+              >
+                clear filter
+              </button>
+            </div>
           </div>
         </aside>
 
@@ -165,30 +196,38 @@ const LostAndFoundReport = () => {
             {isLoadingReports ? (
               <h1>Fetching reports...</h1>
             ) : (
-              <>{
-                filteredReports.length>0 ? <> {
-                  filteredReports?.map((item, index) => (
-                <div
-                  onClick={() => navigate(`/item/${item._id}`)}
-                  key={index}
-                  className="outline text-white bg-[#18192f] outline-[.1px] p-2 rounded-md"
-                >
-                  <h3 className="text-sm md:text-base text-white font-bold">
-                    {item.itemType}
-                  </h3>
-                  <p className="text-xs">Reported on: {item.foundOn}</p>
-                  <p className="text-sm">
-                    Status:{" "}
-                    <span  className={`font-extrabold ${
-                      item.reportStatus === "FOUND" ? "text-green-400" : "text-red-500"
-                    }`}>{item.reportStatus}</span>
-                  </p>
-                </div>
-              ))
-                }</>:<h1>
-                  no data found
-                </h1>
-              }
+              <>
+                {filteredReports.length > 0 ? (
+                  <>
+                    {" "}
+                    {filteredReports?.map((item, index) => (
+                      <div
+                        onClick={() => navigate(`/item/${item._id}`)}
+                        key={index}
+                        className="outline cursor-pointer text-white bg-[#18192f] outline-[.1px] p-2 rounded-md"
+                      >
+                        <h3 className="text-sm md:text-base text-white font-bold">
+                          {item.itemType}
+                        </h3>
+                        <p className="text-xs">Reported on: {item.foundOn}</p>
+                        <p className="text-sm">
+                          Status:{" "}
+                          <span
+                            className={`font-extrabold ${
+                              item.reportStatus === "FOUND"
+                                ? "text-green-400"
+                                : "text-red-500"
+                            }`}
+                          >
+                            {item.reportStatus}
+                          </span>
+                        </p>
+                      </div>
+                    ))}
+                  </>
+                ) : (
+                  <h1>no data found</h1>
+                )}
               </>
             )}
           </div>
@@ -218,12 +257,17 @@ const LostAndFoundReport = () => {
             onSubmit={handleSubmit}
             className="relative bg-black/20 border-[.1px] p-6 rounded-lg w-full max-w-md sm:max-w-xl shadow-lg z-50"
           >
-            <h2 className="text-xl text-white font-bold mb-4">Report an Item</h2>
+            <h2 className="text-xl text-white font-bold mb-4">
+              Report an Item
+            </h2>
 
             <div className="grid grid-cols-2 gap-4">
               <div>
-                <label htmlFor="itemType" className="block text-white font-medium">
-                  Item Type:
+                <label
+                  htmlFor="itemType"
+                  className="block text-white font-medium"
+                >
+                  Item Name:
                 </label>
                 <input
                   type="text"
@@ -236,7 +280,10 @@ const LostAndFoundReport = () => {
                 />
               </div>
               <div>
-                <label htmlFor="location" className="block text-white font-medium">
+                <label
+                  htmlFor="location"
+                  className="block text-white font-medium"
+                >
                   Location:
                 </label>
                 <input
@@ -250,7 +297,10 @@ const LostAndFoundReport = () => {
                 />
               </div>
               <div>
-                <label htmlFor="specification" className="block text-white font-medium">
+                <label
+                  htmlFor="specification"
+                  className="block text-white font-medium"
+                >
                   Specification:
                 </label>
                 <input
@@ -264,21 +314,34 @@ const LostAndFoundReport = () => {
                 />
               </div>
               <div>
-                <label htmlFor="category" className="block text-white font-medium">
+                <label
+                  htmlFor="category"
+                  className="block text-white font-medium"
+                >
                   Category:
                 </label>
-                <input
-                  type="text"
+                <select
                   id="category"
                   name="category"
                   value={formData.category}
                   onChange={handleChange}
                   required
                   className="border p-2 rounded w-full"
-                />
+                >
+                  <option value="" disabled selected>
+                    Select a category
+                  </option>
+                  <option  value="accessories">Accessories</option>
+                  <option value="electronics">Electronics</option>
+                  <option value="other">Other</option>
+                </select>
               </div>
+
               <div>
-                <label htmlFor="foundOn" className="block text-white font-medium">
+                <label
+                  htmlFor="foundOn"
+                  className="block text-white font-medium"
+                >
                   Found On:
                 </label>
                 <input
@@ -292,7 +355,10 @@ const LostAndFoundReport = () => {
                 />
               </div>
               <div>
-                <label htmlFor="contact" className="block text-white font-medium">
+                <label
+                  htmlFor="contact"
+                  className="block text-white font-medium"
+                >
                   Contact Info:
                 </label>
                 <input
@@ -305,21 +371,51 @@ const LostAndFoundReport = () => {
                   className="border p-2 rounded w-full"
                 />
               </div>
+              <div className="mt-3 mb-3 flex  ">
+                <label htmlFor="Found" className="block text-white font-medium">
+                  Found
+                </label>
+                <input
+                  type="radio"
+                  id="Found"
+                  name="reportStatus"
+                  value="FOUND"
+                  onChange={handleChange}
+                  required
+                  className="border  p-2 rounded w-full"
+                />
+                <label htmlFor="Lost" className="block text-white font-medium">
+                  Lost
+                </label>
+                <input
+                  type="radio"
+                  id="Lost"
+                  name="reportStatus"
+                  value="LOST"
+                  onChange={handleChange}
+                  required
+                  className="border p-2 rounded w-full"
+                />
+              </div>
             </div>
-         <div className="mt-4 flex justify-end">
-         <button
-             onClick={()=>setIsReporting(false)}
-              className="mr-4 py-3 px-4 bg-red-500 text-white font-bold rounded"
-            >
-              cancel
-            </button>
-            <button
-              type="submit"
-              className="  py-3 px-4 bg-green-500 text-white font-bold rounded"
-            >
-              Submit
-            </button>
-         </div>
+            <div className="mt-4 flex justify-end">
+              <button
+                onClick={() => setIsReporting(false)}
+                className="mr-4 py-3 px-4 bg-red-500 text-white font-bold rounded"
+              >
+                cancel
+              </button>
+              <button
+                type="submit"
+                className="  py-3 px-4 bg-green-500 text-white font-bold rounded"
+              >
+                {
+                  addingReport ? <span>adding..</span>:<span>
+                  Submit
+                  </span>
+                }
+              </button>
+            </div>
           </form>
         </div>
       )}
